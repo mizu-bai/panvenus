d83da42db5c0ad8681a94f4d4ea40d47f911df29
mizu-bai <shiragawa4519@outlook.com>
2024-01-29 22:10:33 +0800

    Update

---
+++ pyvenus/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="pyvenus",
+    version="0.1.0",
+    author="mizu-bai",
+    author_email="shiragawa4519@outlook.com",
+    description="Read VENUS96 output.",
+    url="https://github.com/mizu-bai/panvenus",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy",
+    ],
+)
